2d3ff67df20584f2baed8a41b9a9c94c8d9b0a66
Fabian Jakobs <fabian.jakobs@web.de>
2010-08-09 10:09:00 +0200

    add async testing demo

--- demo/test.js
+++ demo/test.js
@@ -1,7 +1,11 @@
+var assert = require("assert")
 
 var suite = {
-    setUp: function(next) {
-        
+    setUp: function() {
+        next()
+    },
+    
+    tearDown: function() {
     },
     
     testSyncOK: function() {
@@ -13,10 +17,21 @@ var suite = {
             assert.ok(true);
             next();
         }, 0)
-    }
+    },
+    
+    testSyncFail: function() {
+        assert.ok(false)
+    },
+    
+    testAsyncFail: function(next) {
+        setTimeout(function() {
+            assert.ok(false);
+            next();
+        }, 0)
+    },
 }
 
 var atest = require("../lib/atest")
-atest.suite(suite).run(function() {
-    console.log("DONE")
-})
+atest.suite(suite).run().report().end();
+
+// filter
