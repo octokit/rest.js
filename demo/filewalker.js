d836b2f38ff4a8a86b6e5fbfc631ad590390069a
Fabian Jakobs <fabian.jakobs@web.de>
2010-08-11 00:55:08 +0200

    implement copydir

--- demo/filewalker.js
+++ demo/filewalker.js
@@ -1,5 +1,7 @@
 
 var async = require("../lib/async")
+var fs = require("fs")
+var path = require("path")
 
 function postOrder(next) {
     console.log("")
@@ -34,4 +36,74 @@ function filerAssets(next) {
         .end(next)
 }
 
+// TODO use read and write to make copy streaming
+// TODO set the mode of the new file
+function copyfile(srcPath, destPath, callback) {
+    fs.stat(destPath, function(err, stat) {
+        if (stat && stat.isDirectory())
+            destPath = path.join(destPath, path.basename(srcPath))
+        copy()
+    })
+    
+    function copy() {
+        fs.readFile(srcPath, function(err, data) {
+            if (err)
+                return callback(err)
+            
+            fs.writeFile(destPath, data, callback)
+        })
+    }
+}
+
+abspath = function(dir) {
+    dir = path.normalize(dir)
+    if (dir.charAt(0) == "/")
+        return dir
+    else
+        return path.normalize(path.join(process.cwd(), dir))
+}
+
+function copydir(srcPath, destPath, callback) {
+    srcPath = abspath(srcPath)
+    destPath = abspath(destPath)
+
+    if (destPath.indexOf(srcPath) == 0 && destPath.charAt(srcPath.length) == "/")
+        return callback("the destination path is inside of the source path")
+    
+    path.exists(destPath, function(exists) {
+        if (!exists)
+            fs.mkdir(destPath, 0755, walk)
+        else
+            walk()
+    })
+    
+    function walk(err) {
+        if (err)
+            return callback(err)
+            
+        async.walkfiles(srcPath, null, true)
+            .stat()
+            .each(function(file, next) {
+                var relative = file.path.substring(srcPath.length)
+                if (!relative)
+                    return next();
+                
+                var dest = path.join(destPath, relative)
+                if (file.stat.isDirectory())
+                    fs.mkdir(dest, file.stat.mode, next)
+                else
+                    copyfile(file.path, dest, next)
+            })
+            .end(callback)
+    }
+}
+
+
+function copy(next) {
+    copydir(__dirname, __dirname + "/../COPY", function(err) {
+        console.log("DONE " + err)
+    })
+}
+
+//copy(function() {})
 async.list([postOrder, inOrder, filerAssets]).call().end()
