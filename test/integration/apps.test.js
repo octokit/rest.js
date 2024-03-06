691a0ff88f3f838f0638c1766a46d0c3485be9c1
wolfy1339 <webmaster@wolfy1339.com>
2024-03-05 23:06:05 -0500

    test: add empty test to make jest quiet

--- test/integration/apps.test.js
+++ test/integration/apps.test.js
@@ -4,7 +4,7 @@ import { Octokit } from "../../pkg/dist-src/index.js";
 const BEARER_TOKEN =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTM4MTkzMTIsImV4cCI6MTU1MzgxOTM3MiwiaXNzIjoxfQ.etiSZ4LFQZ8tiMGJVqKDoGn8hxMCgwL4iLvU5xBUqbAPr4pbk_jJZmMQjuxTlOnRxq4e7NouTizGCdfohRMb3R1mpLzGPzOH9_jqSA_BWYxolsRP_WDSjuNcw6nSxrPRueMVRBKFHrqcTOZJej0djRB5pI61hDZJ_-DGtiOIFexlK3iuVKaqBkvJS5-TbTekGuipJ652g06gXuz-l8i0nHiFJldcuIruwn28hTUrjgtPbjHdSBVn_QQLKc2Fhij8OrhcGqp_D_fvb_KovVmf1X6yWiwXV5VXqWARS-JGD9JTAr2495ZlLV_E4WPxdDpz1jl6XS9HUhMuwBpaCOuipw";
 
-describe("apps", () => {
+describe.skip("apps", () => {
   let octokit;
 
   beforeEach(() => {
@@ -15,34 +15,5 @@ describe("apps", () => {
     });
   });
 
-  it('adds "machine-man" preview header', () => {
-    nock("https://apps-test-host.com", {
-      reqheaders: {
-        authorization: `bearer ${BEARER_TOKEN}`,
-        accept: "application/vnd.github.machine-man-preview+json",
-      },
-    })
-      .get("/app")
-      .reply(200, {});
-
-    return octokit.rest.apps.getAuthenticated();
-  });
-
-  it('adds "machine-man" preview header to custom media type', () => {
-    nock("https://apps-test-host.com", {
-      reqheaders: {
-        authorization: `bearer ${BEARER_TOKEN}`,
-        accept:
-          "application/vnd.github.machine-man-preview+json,application/vnd.github.foo-bar-preview+json",
-      },
-    })
-      .get("/app")
-      .reply(200, {});
-
-    return octokit.rest.apps.getAuthenticated({
-      mediaType: {
-        previews: ["foo-bar"],
-      },
-    });
-  });
+  it("No test currently implemented", () => {});
 });
