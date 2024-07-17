c9b5f6e96610ab0367181e9a3ab0d02dc203c0c7
uzlopak <aras.abbasi@googlemail.com>
2024-07-17 16:09:19 +0200

    enable more

--- test/integration/apps.test.ts
+++ test/integration/apps.test.ts
@@ -1,17 +1,17 @@
-const nock = require("nock");
+import { describe, beforeEach, it } from "vitest";
+import nock from "nock";
+import { Octokit } from "../../src/index.ts";
 
-const { Octokit } = require("../../");
 const BEARER_TOKEN =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTM4MTkzMTIsImV4cCI6MTU1MzgxOTM3MiwiaXNzIjoxfQ.etiSZ4LFQZ8tiMGJVqKDoGn8hxMCgwL4iLvU5xBUqbAPr4pbk_jJZmMQjuxTlOnRxq4e7NouTizGCdfohRMb3R1mpLzGPzOH9_jqSA_BWYxolsRP_WDSjuNcw6nSxrPRueMVRBKFHrqcTOZJej0djRB5pI61hDZJ_-DGtiOIFexlK3iuVKaqBkvJS5-TbTekGuipJ652g06gXuz-l8i0nHiFJldcuIruwn28hTUrjgtPbjHdSBVn_QQLKc2Fhij8OrhcGqp_D_fvb_KovVmf1X6yWiwXV5VXqWARS-JGD9JTAr2495ZlLV_E4WPxdDpz1jl6XS9HUhMuwBpaCOuipw";
 
 describe("apps", () => {
-  let octokit;
+  let octokit: Octokit;
 
   beforeEach(() => {
     octokit = new Octokit({
       baseUrl: "https://apps-test-host.com",
       auth: `Bearer ${BEARER_TOKEN}`,
-      log: console.log,
     });
   });
 
@@ -19,7 +19,7 @@ describe("apps", () => {
     nock("https://apps-test-host.com", {
       reqheaders: {
         authorization: `bearer ${BEARER_TOKEN}`,
-        accept: "application/vnd.github.machine-man-preview+json",
+        accept: "application/vnd.github.v3+json",
       },
     })
       .get("/app")
@@ -32,8 +32,7 @@ describe("apps", () => {
     nock("https://apps-test-host.com", {
       reqheaders: {
         authorization: `bearer ${BEARER_TOKEN}`,
-        accept:
-          "application/vnd.github.machine-man-preview+json,application/vnd.github.foo-bar-preview+json",
+        accept: "application/vnd.github.v3+json",
       },
     })
       .get("/app")
